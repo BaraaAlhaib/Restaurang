4eb913d8f206a8f9667ee185359d06f3884fe734
Lenovo <Lenovo@LAPTOP-KILFEO10>
2024-02-27 16:21:10 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿string employeeName = "";
+double employeeSalary = 0;
+
+Console.WriteLine("Employee System");
+Console.WriteLine("--------------------------");
+
+Console.WriteLine("Enter employee name:");
+employeeName = Console.ReadLine();
+
+Console.WriteLine("Enter employee salary:");
+employeeSalary = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Name: {employeeName}, Salary: {employeeSalary}");
